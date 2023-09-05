dd79c9c566e0818c59df1f188c8e44df93ca803a
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-09-06 01:37:57 +0500

    Get data

--- app/page.js
+++ app/page.js
@@ -4,43 +4,45 @@ import { AiTwotoneDelete } from 'react-icons/ai';
 import Navbar from './(components)/Navbar';
 
 export default function Home() {
-  const [data, setData] = useState([]); // Initialize data as an empty array
+  const [data, setData] = useState([]); 
   const [userName, setUserName] = useState('');
-  const [userName1, setUserName1] = useState([]);
 
-  // var userName1=[]
-
-  
-  const handleChange = (e) => {
+    
+  const handleChange =  (e) => {
     setUserName(e.target.value);
   };
-  const handleGet =   () => {
-    // console.log(userName);
-     setUserName1( [...userName1,userName])
-     
-
-  // console.log(userName1);
-    // try {
-    //   const response = await fetch(`https://api.github.com/users/${userName}`);
-    //   if (!response.ok) {
-    //     alert('User not found'); 
-    //     return// Handle error if user not found
-    //   }
-    //   const userData = await response.json();
-    //   setData([userData]); // Convert the user data into an array and update data
-    // } catch (error) {
-    //   console.error(error.message);
-    // }
+  const handleGet = async  () => {
+        try {
+      const response = await fetch(`https://api.github.com/users/${userName}`);
+      if (!response.ok) {
+        alert('User not found'); 
+        setUserName('')
+        return
+      }
+      setUserName('')
+      const userData = await response.json();
+      setData([...data, userData]);
+      setUserName('')
+    } catch (error) {
+      console.error(error.message);
+    }
   };
+
+  const handleDelete = (username) =>{
+    let userInfo = data.filter((item)=>{
+      if(item.login !== username){
+        return item
+      }
+    })
+    setData(userInfo)
+       
+  }
 
 
   return (
     <>
       <Navbar />
-         {userName1.map((item,i)=>{
-          return <div key={i} className='text-center'>{item}</div>
-         })}
-      <div className='text-center mt-10'>
+        <div className='text-center mt-10'>
         <label>Write GitHub Username and get Data 🌎</label>
         <br />
         <br />
@@ -49,13 +51,14 @@ export default function Home() {
           placeholder='Username'
           name='username'
           type="text"
-          className='w-1/4 py-3 px-3 rounded-md focus:outline-blue-200'
+          value={userName}
+          className='lg:w-[50vh] md:w-[50vh] w-2/3 py-3 px-3 rounded-md focus:outline-blue-200'
         />
         <br />
-        <button onClick={handleGet} className='py-2 px-3 text-white font-bold bg-blue-700 rounded-md m-5'>Get Data</button>
+        <button onClick={handleGet} className='w-[30vh] py-2 px-3 text-white font-bold bg-blue-700 rounded-md m-5'>Get Data</button>
       </div>
 
-      {/* <div className="flex flex-col m-10">
+      <div className="flex flex-col m-10">
         <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
             <div className="overflow-hidden">
@@ -67,6 +70,7 @@ export default function Home() {
                       <th scope="col" className="px-6 py-4">Name</th>
                       <th scope="col" className="px-6 py-4">Followers</th>
                       <th scope="col" className="px-6 py-4">Following</th>
+                      <th scope="col" className="px-6 py-4">Github</th>
                       <th scope="col" className="px-6 py-4">Action</th>
                     </tr>
                   </thead>
@@ -77,8 +81,11 @@ export default function Home() {
                         <td className="whitespace-nowrap px-6 py-4">{userData.login}</td>
                         <td className="whitespace-nowrap px-6 py-4">{userData.followers}</td>
                         <td className="whitespace-nowrap px-6 py-4">{userData.following}</td>
+                       <td>
+                            <a href={userData.html_url} target='blank' className="whitespace-nowrap px-6 py-4">{userData.html_url}</a>
+                        </td> 
                         <td className="whitespace-nowrap px-6 py-4">
-                          <AiTwotoneDelete className='text-3xl cursor-pointer text-red-600'/>
+                          <AiTwotoneDelete onClick={()=>{handleDelete(userData.login)}} className='text-3xl cursor-pointer text-red-600'/>
                         </td>
                       </tr>
                     ))}
@@ -88,7 +95,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
